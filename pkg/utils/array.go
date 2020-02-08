a84b2fb9756fcf31891d7b6e9307f9ce03679d5a
wonktnodi <wonktnodiq@gmail.com>
2020-02-08 15:35:21 +0800

    :new: utils for time and uint64 array

--- pkg/utils/array.go
+++ pkg/utils/array.go
@@ -1,1 +1,14 @@
 package utils
+
+func Uint64ArrayRemoveDuplicated(arr []uint64) (ret []uint64) {
+  var list = map[uint64]bool{}
+  for _, v := range arr {
+    list[v] = true
+  }
+  
+  ret = make([]uint64, 0)
+  for v, _ := range list {
+    ret = append(ret, v)
+  }
+  return
+}
